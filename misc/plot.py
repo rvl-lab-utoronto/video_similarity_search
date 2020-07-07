52ed5033fbd92f702d5084593a4dd09959653990
Salar <salar77h@gmail.com>
2020-07-07 00:50:13 -0400

    Fixed plotting script
    Signed-off-by: Salar <salar77h@gmail.com>

--- misc/plot.py
+++ misc/plot.py
@@ -16,31 +16,31 @@ def plot_training_progress ():
         for row in csv_reader:
             train_losses.append(float(row[0]))
             train_acc.append(float(row[1]))
-    
+
     with open (val_progress_file, newline='') as csvfile:
         csv_reader = csv.reader(csvfile, delimiter=' ')
         for row in csv_reader:
             val_losses.append(float(row[0]))
             val_acc.append(float(row[1]))
-    
+
     plt.subplot(1, 2, 1)
     plt.plot(np.arange(len(train_losses)), train_losses)
-    plt.plot(np.arange(len(train_acc)), train_acc)
+    plt.plot(np.arange(len(val_losses)), val_losses)
     plt.xlabel('Epoch')
-    plt.ylabel('Loss/Accuracy')
-    plt.title('Training loss and accuracy vs. Epoch')
-    plt.legend(['Loss', 'Accuracy'])
+    plt.ylabel('Loss')
+    plt.title('Training and Validation Loss vs. Epoch')
+    plt.legend(['Training', 'Validation'])
 
     plt.subplot(1, 2, 2)
-    plt.plot(np.arange(len(val_losses)), val_losses)
+    plt.plot(np.arange(len(train_acc)), train_acc)
     plt.plot(np.arange(len(val_acc)), val_acc)
     plt.xlabel('Epoch')
-    plt.ylabel('Loss/Accuracy')
-    plt.title('Validation loss and accuracy vs. Epoch')
-    plt.legend(['Loss', 'Accuracy'])
+    plt.ylabel('Accuracy')
+    plt.title('Training and Validation Accuracy vs. Epoch')
+    plt.legend(['Training', 'Validation'])
 
     plt.show()
-    
+
 
 if __name__ == '__main__':
     plot_training_progress()

b6322bf53a1490f8a0e367e83702bdd8dcf033ff
Salar Hosseini <salar77h@cedar1.cedar.computecanada.ca>
2020-07-28 10:25:14 -0700

    cedar slowfast scripts
    Signed-off-by: Salar Hosseini <salar77h@cedar1.cedar.computecanada.ca>

---
+++ shell/cc_cedar_slowfast_ucf_job.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --account=def-florian7_gpu 
+#SBATCH --time=0-23:00:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=10
+#SBATCH --gres=gpu:t4:4
+#SBATCH --mem=48G
+#SBATCH --job-name=slowfast_ucf
+#SBATCH --output=%x-%j.out
+
+cd $SLURM_TMPDIR
+mkdir work_ucf
+cd work_ucf
+tar -xzf /home/salar77h/projects/def-florian7/datasets/UCF101/jpg.tar.gz
+'Extracted jpg.tar.gz'
+
+python /home/salar77h/projects/def-florian7/salar77h/repos/video_similarity_search/train.py --cfg /home/salar77h/projects/def-florian7/salar77h/repos/video_similarity_search/config/custom_configs/slowfast_ucf_cc.yaml --gpu 0,1,2,3 --num_data_workers 4 --batch_size 40 --output /home/salar77h/projects/def-florian7/salar77h/repos/video_similarity_search/output_ucf2-newtemporal

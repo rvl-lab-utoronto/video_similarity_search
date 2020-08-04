aa98a1d989e1ef78d75129f355c70c8efb2ae9b6
Salar <salar77h@gmail.com>
2020-08-04 13:23:27 -0400

    Minor fixes to model visualizing script
    Signed-off-by: Salar <salar77h@gmail.com>

--- visualize.py
+++ visualize.py
@@ -10,13 +10,16 @@ args = arg_parser().parse_args()
 cfg = load_config(args)
 plot_name = input('Please specify a name for the plot: ')
 
-x = torch.zeros(1, 3, 32, 224, 224, dtype=torch.float, requires_grad=False)
+x = torch.zeros(1, 3, 32, cfg.DATA.SAMPLE_SIZE, cfg.DATA.SAMPLE_SIZE, dtype=torch.float, requires_grad=False)
 model=model_selector(cfg)
 
 if cfg.MODEL.ARCH == 'slowfast':
     x = multipathway_input(x, cfg)
 
 out = model(x)
-# print(out)
-model_arch = make_dot(out)
-Source(model_arch).render('{}.png'.format(name))
+model_arch_dot = make_dot(out, params=dict(model.named_parameters()))
+
+out_format = 'svg'
+model_arch_dot.format = out_format
+model_arch_dot.render(plot_name)
+print ('Saved graph visualization to {}.{}'.format(plot_name, out_format))

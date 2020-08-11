6ece0fd63b22d819691cdc3650df17772bb812da
Salar <salar77h@gmail.com>
2020-08-11 11:35:04 -0700

    minor fix to dist metrics in tripletnet
    Signed-off-by: Salar <salar77h@gmail.com>

--- models/triplet_net.py
+++ models/triplet_net.py
@@ -4,7 +4,7 @@ Created by Sherry Chen on Jul 3, 2020
 
 import torch
 import torch.nn as nn
-
+import torch.nn.functional as F
 
 class Tripletnet(nn.Module):
     def __init__(self, embeddingnet, dist_metric):
@@ -12,10 +12,7 @@ class Tripletnet(nn.Module):
         self.embeddingnet = embeddingnet
 
         assert dist_metric in ['cosine', 'euclidean']
-        if dist_metric == 'euclidean':
-            self.dist_fcn = nn.PairwiseDistance(p=2)
-        elif dist_metric == 'cosine':
-            self.dist_fcn = nn.CosineSimilarity(dim=1, eps=1e-8)
+        self.dist_metric = dist_metric
 
 
     def forward(self, x, y, z):
@@ -23,10 +20,10 @@ class Tripletnet(nn.Module):
         embedded_x = self.embeddingnet(x)
         embedded_y = self.embeddingnet(y)
         embedded_z = self.embeddingnet(z)
-        if dist_metric == 'euclidean':
-            dist_a = self.dist_fcn(embedded_x, embedded_y)
-            dist_b = self.dist_fcn(embedded_x, embedded_z)
-        elif dist_metric == 'cosine':
-            dist_a = 1 - self.dist_fcn(embedded_x, embedded_y)
-            dist_b = 1 - self.dist_fcn(embedded_x, embedded_z)
+        if self.dist_metric == 'euclidean':
+            dist_a = F.pairwise_distance(embedded_x, embedded_y, 2)
+            dist_b = F.pairwise_distance(embedded_x, embedded_z, 2)
+        elif self.dist_metric == 'cosine':
+            dist_a = 1 - F.cosine_similarity(embedded_x, embedded_y, dim=1)
+            dist_b = 1 - F.cosine_similarity(embedded_x, embedded_z, dim=1)
         return dist_a, dist_b, embedded_x, embedded_y, embedded_z

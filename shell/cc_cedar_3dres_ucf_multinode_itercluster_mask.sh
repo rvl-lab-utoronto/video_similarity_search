db9c66dbad6afa3e1c144dae09f7f3412bb7dd64
Salar <salar77h@gmail.com>
2021-02-09 21:12:36 -0800

    CC configs and shell scripts
    Signed-off-by: Salar <salar77h@gmail.com>

--- shell/cc_cedar_3dres_ucf_multinode_itercluster_mask.sh
+++ shell/cc_cedar_3dres_ucf_multinode_itercluster_mask.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --account=def-florian7_gpu 
-#SBATCH --time=3-00:00:00
+#SBATCH --time=1-00:00:00
 #SBATCH --nodes=2
 #SBATCH --ntasks-per-node=1
-#SBATCH --job-name=3dres_ucf-replacemask-p05
+#SBATCH --job-name=res_ucf_atten
 #SBATCH --output=%x-%j.out
 #SBATCH --gres=gpu:v100l:4
-#SBATCH --mem=32G
-#SBATCH --cpus-per-task=10
+#SBATCH --mem=48G
+#SBATCH --cpus-per-task=8
 # --wait-all-nodes=1
 
 clush -w $(slurm_hl2hl.py --format PDSH) tar -xzf /home/salar77h/projects/def-florian7/datasets/UCF101/poolnet_new.tar.gz -C $SLURM_TMPDIR
@@ -27,5 +27,5 @@ echo $MASTER_ADDRESS
 MPORT=3456
 echo $MPORT
 
-srun python /home/salar77h/projects/def-florian7/salar77h/repos/video_similarity_search/online_train.py --iterative_cluster --cfg /home/salar77h/projects/def-florian7/salar77h/repos/video_similarity_search/config/custom_configs/resnet_ucf_itercluster_mask_cc.yaml --gpu 0,1,2,3 --num_data_workers 4 --batch_size 60 --output /home/salar77h/projects/def-florian7/salar77h/repos/video_similarity_search/output_ucf13-replacemask-p05 --num_shards 2 --ip_address_port tcp://$MASTER_ADDRESS:$MPORT --compute_canada --epoch 800 DATASET.PROB_POS_CHANNEL_REPLACE 0.5 
-#--checkpoint_path /home/salar77h/projects/def-florian7/salar77h/repos/video_similarity_search/output_ucf12-itercluster-inter5-lr05/tnet_checkpoints/3dresnet/checkpoint.pth.tar
+srun python /home/salar77h/projects/def-florian7/salar77h/repos/video_similarity_search/online_train.py --iterative_cluster --cfg /home/salar77h/projects/def-florian7/salar77h/repos/video_similarity_search/config/custom_configs/resnet_ucf_itercluster_mask_cc.yaml --gpu 0,1,2,3 --num_data_workers 4 --batch_size 100 --output /home/salar77h/projects/def-florian7/salar77h/repos/video_similarity_search/output_ucf15 --num_shards 2 --ip_address_port tcp://$MASTER_ADDRESS:$MPORT --compute_canada --epoch 801 
+#--checkpoint_path /home/salar77h/projects/def-florian7/salar77h/repos/video_similarity_search/output_ucf14-itercluster-4d-bn-attention-new/tnet_checkpoints/3dresnet/checkpoint.pth.tar

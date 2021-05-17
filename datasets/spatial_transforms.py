d77ce0a6d5e1f2f574a28e3b0ccee2c022ef65d2
sherrychen127 <sherrychen127@gmail.com>
2021-05-16 22:10:26 -0400

    added finetuning

--- datasets/spatial_transforms.py
+++ datasets/spatial_transforms.py
@@ -174,7 +174,7 @@ class RandomResizedCrop(transforms.RandomResizedCrop):
                  size,
                  scale=(0.08, 1.0),
                  ratio=(3. / 4., 4. / 3.),
-                 interpolation=Image.BILINEAR):
+                 interpolation=transforms.InterpolationMode.BILINEAR):
         super().__init__(size, scale, ratio, interpolation)
         self.randomize_parameters()
 
@@ -189,6 +189,22 @@ class RandomResizedCrop(transforms.RandomResizedCrop):
     def randomize_parameters(self):
         self.randomize = True
 
+class Lambda:
+    """Apply a user-defined lambda as a transform. This transform does not support torchscript.
+    Args:
+        lambd (function): Lambda/function to be used for transform.
+    """
+
+    def __init__(self, lambd):
+        if not callable(lambd):
+            raise TypeError("Argument lambd should be callable, got {}".format(repr(type(lambd).__name__)))
+        self.lambd = lambd
+
+    def __call__(self, img):
+        return self.lambd(img)
+
+    def __repr__(self):
+        return self.__class__.__name__ + '()'
 
 class ColorJitter(transforms.ColorJitter):
 
@@ -201,8 +217,19 @@ class ColorJitter(transforms.ColorJitter):
     def __call__(self, img):
         if self.randomize:
             self.apply = self.random_p < self.p
-            self.transform = self.get_params(self.brightness, self.contrast,
-                                              self.saturation, self.hue)
+            # self.transform = self.get_params(self.brightness, self.contrast,
+            #                                   self.saturation, self.hue)
+            fn_idx, b, c, s, h = self.get_params(self.brightness, self.contrast,
+                                                self.saturation, self.hue)
+            # based on pre-torch1.8 version of get_params: https://github.com/pytorch/vision/blob/2f40a483d73018ae6e1488a484c5927f2b309969/torchvision/transforms/transforms.py#L1053-L1085	
+            transforms = []
+            transforms.append(Lambda(lambda img: F.adjust_brightness(img, b)))
+            transforms.append(Lambda(lambda img: F.adjust_contrast(img, c)))
+            transforms.append(Lambda(lambda img: F.adjust_saturation(img, s)))
+            transforms.append(Lambda(lambda img: F.adjust_hue(img, h)))
+            transforms = [transforms[i] for i in fn_idx]
+            self.transform = Compose(transforms)
+
             self.randomize = False
 
         if self.apply:

eebdadb6b0a261c6bf83db20870ce2c0ae28c6a5
Salar <salar77h@gmail.com>
2021-06-01 18:07:18 -0400

    remove warning suppresion
    Signed-off-by: Salar <salar77h@gmail.com>

--- online_train.py
+++ online_train.py
@@ -3,10 +3,10 @@ Created by Sherry Chen on Jul 3, 2020
 Build and Train Triplet network. Supports saving and loading checkpoints,
 """
 
-def warn(*args, **kwargs):
-        pass
-import warnings
-warnings.warn = warn
+#def warn(*args, **kwargs):
+#        pass
+#import warnings
+#warnings.warn = warn
 
 import sys, os
 #import gc
